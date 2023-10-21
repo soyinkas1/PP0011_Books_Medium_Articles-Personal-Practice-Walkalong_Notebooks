491adbc3f72e41817296effa8019866c354046a0
unknown <soyinkas1@gmail.com>
2023-10-21 15:11:05 +0100

    LPTRW cpt14 continued

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ def test(did_pass):
     """ Print the results of a test"""
     linenum = sys._getframe(1).f_lineno # Get the caller's line number
     if did_pass:
-        msg = 'Test at the line  {0} ok'.format(linenum)
+        msg = 'Test at the line  git {0} ok'.format(linenum)
     else:
         msg = 'Test at the line  {0} FAILED'.format(linenum)
     print(msg)

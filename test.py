04434bfaa4beab5979035de2ac7a299aeb18f73f
unknown <soyinkas1@gmail.com>
2023-10-23 21:06:15 +0100

    LPTRW chpts 14-15-16 OOP PyGame added

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import sys
 
+
 def test(did_pass):
     """ Print the results of a test"""
-    linenum = sys._getframe(1).f_lineno # Get the caller's line number
+    linenumber = sys._getframe(1).f_lineno  # Get the caller's line number
     if did_pass:
-        msg = 'Test at the line  git {0} ok'.format(linenum)
+        msg = 'Test at the line {0} ok'.format(linenumber)
     else:
-        msg = 'Test at the line  {0} FAILED'.format(linenum)
+        msg = 'Test at the line {0} FAILED'.format(linenumber)
     print(msg)
